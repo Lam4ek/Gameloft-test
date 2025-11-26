b2b223b0ee3ea90574b90ba08abf7fd789bd1259
Lam4ek <d.i.iljyusha@student.khai.edu>
2025-11-26 19:11:03 +0100

    Vite is initialized

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='min-h-screen bg-gray-50 p-6'>
+      <h1 className='text-2xl font-semibold'>Gameloft Frontend Test</h1>
+    </div>
+  );
+}
+
+export default App;
